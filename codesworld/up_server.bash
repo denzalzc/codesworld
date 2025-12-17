6debb085eca9626abcb21d1f1b2f26ccdcbd5e7e
denzalzc <deniszabolzc@gmail.com>
2025-12-17 21:42:39 +0400

    init

--- codesworld/up_server.bash
+++ codesworld/up_server.bash
@@ -1,5 +1,6 @@
 cp nginx.art /etc/nginx/sites-available/codesworld
 nginx -t
+sudo ln -s /etc/nginx/sites-available/codesworld /etc/nginx/sites-enabled/
 .././venv/bin/python3 manage.py collectstatic
 DJANGO_DEBUG=False
 .././venv/bin/python3 .././venv/bin/gunicorn --bind 127.0.0.1 codesworld.wsgi:application
